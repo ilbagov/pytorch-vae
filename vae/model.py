e4093132172760a6f877f08ceb19f7011cb57630
ilbagov <ilia.bagov@gmail.com>
2021-01-27 13:52:41 +0100

    Update model.py
    Added a VAE class. Need to refactor!

--- vae/model.py
+++ vae/model.py
@@ -1,5 +1,7 @@
+import torch
 import torch.nn as nn
 import numpy as np
+from torch.distributions.multivariate_normal import MultivariateNormal
 
 H_IN = 512
 W_IN = H_IN
@@ -48,12 +50,12 @@ class Encoder(nn.Module):
             w_out = np.floor((w_out + 2 * CONV_PADDING - (CONV_KERNEL_SIZE - 1) - 1) + 1)  # width reduction due to Conv2d
             w_out = np.floor((w_out - (POOL_KERNEL_SIZE - 1) - 1) / POOL_STRIDE + 1)  # width reduction due to MaxPool2d
 
-        self.encoder = nn.Sequential(*layers, nn.Flatten())
+        self.encoder_net = nn.Sequential(*layers, nn.Flatten())
         self.mu_layer = nn.Linear(int(h_out)*int(w_out)*hidden_dims, dim_z)
         self.var_layer = nn.Linear(int(h_out)*int(w_out)*hidden_dims, dim_z)
 
     def forward(self, x):
-        x = self.encoder(x)
+        x = self.encoder_net(x)
         mu = self.mu_layer(x)
         var = self.var_layer(x)
 
@@ -62,30 +64,82 @@ class Encoder(nn.Module):
 
 class Decoder(nn.Module):
 
-    def __init__(self):
+    def __init__(self, num_hidden_layers, dim_z):
         super(Decoder, self).__init__()
-        pass
+
+        hidden_dims = INIT_OUT_CHANNELS
+        h_out = H_IN
+        w_out = W_IN
+
+        h_out = np.floor((h_out + 2 * CONV_PADDING - (CONV_KERNEL_SIZE - 1) - 1) + 1)  # height reduction due to Conv2d
+        h_out = np.floor((h_out - (POOL_KERNEL_SIZE - 1) - 1) / POOL_STRIDE + 1)  # height reduction due to MaxPool2d
+
+        w_out = np.floor((w_out + 2 * CONV_PADDING - (CONV_KERNEL_SIZE - 1) - 1) + 1)  # width reduction due to Conv2d
+        w_out = np.floor((w_out - (POOL_KERNEL_SIZE - 1) - 1) / POOL_STRIDE + 1)  # width reduction due to MaxPool2d
+
+        for i in range(num_hidden_layers-1):
+            hidden_dims *= 2
+
+            h_out = np.floor((h_out + 2 * CONV_PADDING - (CONV_KERNEL_SIZE - 1) - 1) + 1)  # height reduction due to Conv2d
+            h_out = np.floor((h_out - (POOL_KERNEL_SIZE - 1) - 1) / POOL_STRIDE + 1)  # height reduction due to MaxPool2d
+
+            w_out = np.floor((w_out + 2 * CONV_PADDING - (CONV_KERNEL_SIZE - 1) - 1) + 1)  # width reduction due to Conv2d
+            w_out = np.floor((w_out - (POOL_KERNEL_SIZE - 1) - 1) / POOL_STRIDE + 1)  # width reduction due to MaxPool2d
+
+        self.input_layer = nn.Linear(dim_z, int(h_out)*int(w_out)*hidden_dims)
+
+        self.h_input = int(h_out)
+        self.w_input = int(w_out)
+        self.channels_input = int(hidden_dims)
+
+        hidden_dims_decoder = hidden_dims
+        layers = nn.ModuleList([nn.ConvTranspose2d(in_channels=int(hidden_dims_decoder),
+                                                   out_channels=int(hidden_dims_decoder/2),
+                                                   kernel_size=CONV_KERNEL_SIZE, padding=CONV_PADDING,
+                                                   stride=2, output_padding=1),
+                                nn.BatchNorm2d(num_features=int(hidden_dims_decoder/2)),
+                                nn.ReLU()])
+
+        for i in range(num_hidden_layers-1):
+            hidden_dims_decoder = hidden_dims_decoder/2
+            layers.extend([nn.ConvTranspose2d(in_channels=int(hidden_dims_decoder),
+                                              out_channels=int(hidden_dims_decoder/2),
+                                              kernel_size=CONV_KERNEL_SIZE, padding=CONV_PADDING,
+                                              stride=2, output_padding=1),
+                           nn.BatchNorm2d(num_features=int(hidden_dims_decoder/2)),
+                           nn.ReLU()])
+
+        # add final layer
+        layers.extend([nn.Conv2d(in_channels=int(hidden_dims_decoder/2), out_channels=3,
+                                 kernel_size=CONV_KERNEL_SIZE, padding=CONV_PADDING),
+                       nn.Tanh()])
+
+        self.decoder_net = nn.Sequential(*layers)
 
     def forward(self, x):
-        pass
+        x = self.input_layer(x)
+        x = x.view(-1, self.channels_input, self.w_input, self.h_input)
+        x = self.decoder_net(x)
+
+        return x
 
 
 class VAE(nn.Module):
 
-    def __init__(self):
+    def __init__(self, num_hidden_layers, dim_z):
         super(VAE, self).__init__()
+        self.encoder = Encoder(num_hidden_layers, dim_z)
+        self.decoder = Decoder(num_hidden_layers, dim_z)
+        self.dist = MultivariateNormal(loc=torch.ones(dim_z), covariance_matrix=torch.eye(dim_z))
         pass
 
     def forward(self, x):
+        # TODO differentiate between training/inference
         mu, sigma = self.encoder(x)
-        epsilon = 0 # TODO: implement sampling
+        epsilon = self.dist.rsample()
 
         z = mu + sigma*epsilon
         x = self.decoder(z)
 
         return x
 
-    def forward_trained(self, z):
-        x = self.decoder(z)
-
-        return z
